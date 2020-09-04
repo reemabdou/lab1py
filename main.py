26ae64dce5c25aa692bca811347ca5c383d1cf4d
reemabdou <rpa5285@psu.edu>
2020-09-04 02:06:28 +0000

    space

--- main.py
+++ main.py
@@ -6,10 +6,10 @@
 temperature = float(input("Enter temperature: "))
 unit = input("Enter unit in F/f or C/c: ")
 if unit == "F" or unit == "f":
-  tcelsius = float((temperature - 32) * (5/9))
-  print (f"{temperature}° in Fahrenheit is equivalent to {tcelsius}° Celsius.")
+   tcelsius = float((temperature - 32) * (5/9))
+   print (f"{temperature}° in Fahrenheit is equivalent to{tcelsius}° Celsius.")
 elif unit == "C" or unit == "c":
-  tfahrenheit = float(temperature * 9/5 + 32)
-  print (f"{temperature}° in Celsius is equivalent to {tfahrenheit}° Fahrenheit.")
+   tfahrenheit = float(temperature * 9/5 + 32)
+   print (f"{temperature}° in Celsius is equivalent to{tfahrenheit}° Fahrenheit.")
 else:
   print(f"Invalid unit({unit}).")
