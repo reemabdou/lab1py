72589a2c99441e85ceebce4a0eb9bdebc98ed93e
reemabdou <rpa5285@psu.edu>
2020-09-04 01:16:02 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,15 @@
+# Author: Reem Abdou rpa5285@psu.edu
+# Collaborator: Purushottam Shulka pps5338@psu.edu
+# Collaborator: Tristan Zanowic tsz5030@psu.edu
+# Collaborator: Andrew Torri abt5506@psu.edu
+
+temperature = int(input("Enter temperature: "))
+unit = input("Enter unit in F/f or C/c: ")
+if unit == "F" or unit == "f":
+ tcelsius = float((temperature - 32) * float(5/9))
+ print (f"{temperature}° in Fahrenheit is equivalent to {tcelsius}° Celsius. " )
+elif unit == "C" or unit == "c": 
+  tfahrenheit = float((temperature * float(9/5)) + 32)
+  print (f"{temperature}° in Celsius is equivalent to {tfahrenheit}° Fahrenheit. " )
+else: 
+  print(f"Invalid unit({unit}).")
